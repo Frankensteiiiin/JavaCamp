66768a56cab9884e18c72d4ad1a8e6b10293a171
Frankensteiiiin <muratctopcu@gmail.com>
2022-11-23 14:47:37 +0300

    DayFour

---
+++ DayFour/Devs/src/main/java/kodlama/io/Devs/DevsApplication.java
@@ -0,0 +1,16 @@
+package kodlama.io.Devs;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DevsApplication {
+
+	public static void main(String[] args) throws Exception {
+		SpringApplication.run(DevsApplication.class, args);
+		
+		
+		
+	}
+
+}
